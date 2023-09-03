643216a3144b691cde8e9ff77884f770a4bccade
kubq01 <kuba18062001@gmail.com>
2023-09-03 14:55:29 +0200

    initial commit

---
+++ src/main/java/com/example/tripapp/TripAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripAppApplication.class, args);
+	}
+
+}
