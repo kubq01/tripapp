6fbb105e232f0c947aceff6542c08e73e3f62880
kubq01 <kuba18062001@gmail.com>
2023-10-06 14:12:55 +0200

    frontend index and start of the logging system

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from "./login/LoginForm.tsx";
+
+
+function App() {
+  return (
+    <LoginForm/>
+  );
+}
+
+export default App;
