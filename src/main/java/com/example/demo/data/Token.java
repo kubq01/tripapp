4f0d0f3e991d14816e4bc4a7601e252c8dc5f6c1
kubq01 <kuba18062001@gmail.com>
2023-10-06 14:39:24 +0200

    added token entity

---
+++ src/main/java/com/example/demo/data/Token.java
@@ -0,0 +1,24 @@
+package com.example.demo.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private String id;
+    private String token;
+    @OneToOne
+    private User user;
+}
